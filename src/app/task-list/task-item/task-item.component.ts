8a395854670eda64058e6c93111d8c0696f8469c
Jacques <jacquesvanluyck@gmail.com>
2025-03-20 11:04:57 +0100

    add ngClass to task-item.component

--- src/app/task-list/task-item/task-item.component.ts
+++ src/app/task-list/task-item/task-item.component.ts
@@ -1,14 +1,14 @@
 import { Component, input } from '@angular/core';
 import { Task } from '../../models/task';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
  task = input.required<Task>()
-
-
+ 
 }
